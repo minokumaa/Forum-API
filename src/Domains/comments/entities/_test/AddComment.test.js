a765ae24502a9a3c15d2ddd7c984b4ac799dd8f5
Jeremy Christopher <michryan1134@gmail.com>
2024-10-20 20:22:55 +0700

    domain comments

---
+++ src/Domains/comments/entities/_test/AddComment.test.js
@@ -0,0 +1,40 @@
+const AddComment = require('../AddComment');
+
+descibe('AddComment entities', () => {
+    it('should throw error when payload not contain needed property', () => {
+        // Arrange
+        const payload = {};
+
+        // Action & Assert
+        expect(() => new AddComment(payload)).toThrowError('ADD_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+    });
+
+    it('should throw error when payload has invalid data type', () => {
+        // Arrange
+        const payload = {
+            content: true,
+            owner: 123,
+            thread: [],
+        };
+
+        // Action & Assert
+        expect(() => new AddComment(payload)).toThrowError('ADD_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    });
+
+    it('should create AddComment object correctly', () => {
+        // Arrange
+        const payload = {
+            content: 'lorem ipsum',
+            owner: 'user-123',
+            thread: 'thread-123',
+        };
+
+        // Action
+        const addComment = new AddComment(payload);
+
+        // Assert
+        expect(addComment.content).toEqual(payload.content);
+        expect(addComment.owner).toEqual(payload.owner);
+        expect(addComment.thread).toEqual(payload.thread);
+    });
+});
